460a79321c82feb66c2a2cafb13ee4b547221dde
Leonardo <samba25leo@gmail.com>
2021-06-19 13:33:26 -0300

    adv

--- EDScrapper/src/scripts/content_script.js
+++ EDScrapper/src/scripts/content_script.js
@@ -6,6 +6,13 @@ try {
             });
         }
     })
+    window.addEventListener('load', (event) => {
+        if (document.getElementById("vimeo-player") != undefined) {
+            chrome.runtime.sendMessage("getMyInfo", tabId => {
+                console.log(tabId);
+            });
+        }
+    })
 }
 catch (err) {
     console.log(err);
